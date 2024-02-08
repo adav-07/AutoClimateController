95957bbfb26c1afdaaff990165c865b3983cf203
adav-07 <krithikadavm@gmail.com>
2024-02-08 15:58:16 +0530

    Add project files.

---
+++ RegulatorLibrary/Regulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AutoClimateControllerConsoleApp
+{
+    public class Regulator : IRegulator
+    {
+        public void ChangeTemp(double newTemp)
+        {
+            Console.WriteLine($"Temperature changed to: {newTemp}");
+        }
+    }
+}
